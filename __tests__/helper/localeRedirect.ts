355998f8cedf3a529fab4634c201a001251c6c88
Kore Nordmann <mail@kore-nordmann.de>
2022-01-05 10:30:00 +0100

    Anonymized example

--- __tests__/helper/localeRedirect.ts
+++ __tests__/helper/localeRedirect.ts
@@ -4,34 +4,34 @@ describe('determineLocaleFromUrlAndRewriteUrls', () => {
   const expectedRedirects = [
     // Simple domains with a single locale
     { domain: 'localhost-de:3000', pathname: '/', target: '/', determinedLocale: 'de_DE' },
-    { domain: 'tomtailor.de', pathname: '/', target: '/', determinedLocale: 'de_DE' },
-    { domain: 'tomtailor-de.vercel.app', pathname: '/', target: '/', determinedLocale: 'de_DE' },
-    { domain: 'tomtailor.de', pathname: '/damen', target: '/damen', determinedLocale: 'de_DE' },
+    { domain: 'domain.de', pathname: '/', target: '/', determinedLocale: 'de_DE' },
+    { domain: 'domain-de.vercel.app', pathname: '/', target: '/', determinedLocale: 'de_DE' },
+    { domain: 'domain.de', pathname: '/damen', target: '/damen', determinedLocale: 'de_DE' },
     { domain: 'localhost:3000', pathname: '/', target: '/', determinedLocale: 'de_DE' },
 
     { domain: 'localhost-ch:3000', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor-staging-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor.ch', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-staging-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain.ch', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
 
-    { domain: 'tomtailor-staging-ch.vercel.app', pathname: '/de_DE/de/de', target: '/de', determinedLocale: 'de_CH' },
-    { domain: 'tomtailor-staging-ch.vercel.app', pathname: '/de_DE/de', target: '', determinedLocale: 'de_CH' },
+    { domain: 'domain-staging-ch.vercel.app', pathname: '/de_DE/de/de', target: '/de', determinedLocale: 'de_CH' },
+    { domain: 'domain-staging-ch.vercel.app', pathname: '/de_DE/de', target: '', determinedLocale: 'de_CH' },
 
-    { domain: 'tomtailor.ch:3000', pathname: '/de', target: '', determinedLocale: 'de_CH' },
-    { domain: 'tomtailor.ch:3000', pathname: '/de_DE', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor.ch:3000', pathname: '/de_DE/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain.ch:3000', pathname: '/de', target: '', determinedLocale: 'de_CH' },
+    { domain: 'domain.ch:3000', pathname: '/de_DE', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain.ch:3000', pathname: '/de_DE/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
 
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/fr', target: '', determinedLocale: 'fr_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/de/damen', target: '/damen', determinedLocale: 'de_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/fr/damen', target: '/damen', determinedLocale: 'fr_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/damen', target: '/de/damen', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-ch.vercel.app', pathname: '/fr', target: '', determinedLocale: 'fr_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/de/damen', target: '/damen', determinedLocale: 'de_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/fr/damen', target: '/damen', determinedLocale: 'fr_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/damen', target: '/de/damen', determinedLocale: 'de_CH', redirect: true },
 
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/fr', target: '', determinedLocale: 'fr_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/de/damen', target: '/damen', determinedLocale: 'de_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/fr/damen', target: '/damen', determinedLocale: 'fr_CH' },
-    { domain: 'tomtailor-ch.vercel.app', pathname: '/damen', target: '/de/damen', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-ch.vercel.app', pathname: '/', target: '/de/', determinedLocale: 'de_CH', redirect: true },
+    { domain: 'domain-ch.vercel.app', pathname: '/fr', target: '', determinedLocale: 'fr_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/de/damen', target: '/damen', determinedLocale: 'de_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/fr/damen', target: '/damen', determinedLocale: 'fr_CH' },
+    { domain: 'domain-ch.vercel.app', pathname: '/damen', target: '/de/damen', determinedLocale: 'de_CH', redirect: true },
     // */
   ]
 
